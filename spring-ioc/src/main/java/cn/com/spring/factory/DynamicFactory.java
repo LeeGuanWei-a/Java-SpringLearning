c55a6c16810163204f2515ad1652e0eb343037c7
LeeGuanWei-a <leeanthony1998@outlok.com>
2021-07-08 19:26:44 +0800

    第二次提交

--- spring-ioc/src/main/java/cn/com/spring/factory/DynamicFactory.java
+++ spring-ioc/src/main/java/cn/com/spring/factory/DynamicFactory.java
@@ -3,11 +3,10 @@ package cn.com.spring.factory;
 import cn.com.spring.dao.UserDao;
 import cn.com.spring.dao.impl.UserDaoImpl;
 
+//动态工厂实例化对象
+public class DynamicFactory {
 
-//静态工厂实例化对象
-public class StaticFactory {
-
-    public static UserDao getUserDao(){
+    public UserDao getUserDao(){
         return new UserDaoImpl();
     }
 }
