4974924b9fffb81bd5b9d78f02eeb54a28ad6025
LeeGuanWei-a <leeanthony1998@outlok.com>
2021-07-09 19:32:44 +0800

    第四次提交

---
+++ spring-ioc-anno/src/test/java/test/DataSourceTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.junit.Test;
+
+import java.beans.PropertyVetoException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSourceTest {
+
+    //测试手动创建c3p0数据源
+    @Test
+    public void test1() throws PropertyVetoException, SQLException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test1?serverTimezone=UTC");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+}
