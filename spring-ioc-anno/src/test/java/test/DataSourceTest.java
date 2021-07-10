40c98439ec3f296fcab3bac144493329d170a064
LeeGuanWei-a <leeanthony1998@outlok.com>
2021-07-10 11:50:48 +0800

    第四次提交，配置数据源

--- spring-ioc-anno/src/test/java/test/DataSourceTest.java
+++ spring-ioc-anno/src/test/java/test/DataSourceTest.java
@@ -1,22 +1,77 @@
 package test;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class DataSourceTest {
 
-    //测试手动创建c3p0数据源
+    //测试手动创建druid数据源
     @Test
     public void test1() throws PropertyVetoException, SQLException {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test1?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+    //测试手动创建c3p0数据源
+    @Test
+    public void test2() throws PropertyVetoException, SQLException {
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
-        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test1?serverTimezone=UTC");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3307/test1?serverTimezone=UTC");
         dataSource.setUser("root");
         dataSource.setPassword("root");
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+
+    //测试手动创建c3p0数据源(加载properties配置文件形式)
+    @Test
+    public void test3() throws PropertyVetoException, SQLException {
+        //1.读取配置文件
+        ResourceBundle rb = ResourceBundle.getBundle("jdbc");
+        String driver = rb.getString("jdbc.driver");
+        String url = rb.getString("jdbc.url");
+        String username = rb.getString("jdbc.username");
+        String password = rb.getString("jdbc.password");
+
+        //创建数据源对象
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass(driver);
+        dataSource.setJdbcUrl(url);
+        dataSource.setUser(username);
+        dataSource.setPassword(password);
+
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+
+    //测试Spring容器产生数据源对象
+    @Test
+    public void test4() throws PropertyVetoException, SQLException {
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //DataSource dataSource = (DataSource) app.getBean("dataSource");
+        //DataSource dataSource = (DataSource) app.getBean("dataSource2");
+        DataSource dataSource = (DataSource) app.getBean("dataSource3");
+
+        //bean：dataSource唯一时使用
+        //DataSource dataSource = app.getBean(DataSource.class);
         Connection connection = dataSource.getConnection();
         System.out.println(connection);
         connection.close();
